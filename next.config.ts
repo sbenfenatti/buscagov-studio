2b3c3aecbaf0e15fc85ce72d7acec4c266c75f2a
Sérgio Benfenatti <sbenfenatti@me.com>
2025-10-07 18:12:21 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'httpshttps',
+        protocol: 'https',
         hostname: 'i.imgur.com',
         port: '',
         pathname: '/**',
